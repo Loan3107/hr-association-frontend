95c69a74be0f830691955fb84e52358fff049889
PIROTAIS <loan.pirotais@cgi.com>
2023-01-17 13:34:59 +0100

    feat(employees-form): possible to update employee's infos with form

--- src/app/employees/employees-form/employees-form.component.ts
+++ src/app/employees/employees-form/employees-form.component.ts
@@ -1,7 +1,9 @@
-import { Component, EventEmitter, Output } from "@angular/core";
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from "@angular/core";
 import { FormBuilder, Validators } from '@angular/forms';
+import { isNil } from "lodash";
 import { Subscription } from "rxjs";
 import { CreateEmployeeDto } from "src/shared/dtos/create-employee.dto";
+import { PutEmployeeDto } from "src/shared/dtos/put-employee.dto";
 import { Employees } from "src/shared/entities/employees.entity";
 import { EmployeeService } from "src/shared/services/employee.service";
 
@@ -9,16 +11,12 @@ import { EmployeeService } from "src/shared/services/employee.service";
     selector: 'app-employees-form',
     templateUrl: './employees-form.component.html',
 })
-export class EmployeesFormComponent {
+export class EmployeesFormComponent implements OnChanges {
+    @Input('employeeToHandle')
+    public employee!: Employees | undefined;
+
     @Output()
     private submitEvent = new EventEmitter<Employees>();
-
-    private apiSubscription!: Subscription;
-
-    constructor(
-        private readonly formBuilder: FormBuilder,
-        private readonly employeesService: EmployeeService,
-    ) {}
 
     public employeesForm = this.formBuilder.group({
         firstName: ['', Validators.required],
@@ -27,14 +25,57 @@ export class EmployeesFormComponent {
         password: ['', Validators.required],
     });
 
+    private apiSubscription!: Subscription;
+
+    constructor(
+        private readonly formBuilder: FormBuilder,
+        private readonly employeesService: EmployeeService,
+    ) {}
+
+    public ngOnChanges(changes: SimpleChanges): void {
+        const employee = changes['employee'].currentValue;
+        if (!isNil(employee)) {
+            this.employeesForm.patchValue({
+                firstName: employee.firstName,
+                lastName: employee.lastName,
+                mail: employee.mail,
+                password: employee.password,
+            });
+        } else {
+            this.employeesForm.patchValue({
+                firstName: undefined,
+                lastName: undefined,
+                mail: undefined,
+                password: undefined,
+            });
+        }
+    }
+
     public onSubmit(): void {
         if(this.employeesForm.valid) {
-            const employee: CreateEmployeeDto = this.employeesForm.value as CreateEmployeeDto;
-            this.apiSubscription = this.employeesService.create(employee).subscribe({
-                'next': (result) => {
-                    this.submitEvent.emit(result);
-                }
-            });
+            if (isNil(this.employee)) {
+                const employee: CreateEmployeeDto = this.employeesForm.value as CreateEmployeeDto;
+                this.apiSubscription = this.employeesService.create(employee).subscribe({
+                    'next': (result) => {
+                        this.submitEvent.emit(result);
+                    },
+                    'complete': () => {
+                        this.apiSubscription.unsubscribe();
+                    }
+                });
+            } else {
+                const employee: PutEmployeeDto = this.employeesForm.value as PutEmployeeDto;
+                employee.id = this.employee.id;
+
+                this.apiSubscription = this.employeesService.put(this.employee.id, employee).subscribe({
+                    'next': (result) => {
+                        this.submitEvent.emit(result);
+                    },
+                    'complete': () => {
+                        this.apiSubscription.unsubscribe();
+                    }
+                });
+            }
         }
     }
 }
